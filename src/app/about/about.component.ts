6abc753702c6a426598fa79848e9e7e271ae60b3
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-11-22 14:39:43 +0300

    post and error handling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../services/api/products/product.service';
+import { ProductRepresentation } from '../services/api/models/product-representation';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
@@ -19,10 +21,36 @@ export class AboutComponent implements OnInit{
   queryParam: any;
 
   ngOnInit(): void {
-    this.service.getAllProductsWithLimit()
+    const product: ProductRepresentation ={
+      title: 'My Product',
+      description: 'This is a product description',
+      price: 1000,
+      category: 'Any Category',
+      image: 'https://some-image.jpg'
+    }
+    this.service.createProduct(product)
     .subscribe({
-      next: (data) => {console.log(data)}
-    });
+      next: (result: ProductRepresentation) => {
+        console.log(result)
+      },
+      error: (error: HttpErrorResponse) => {
+        console.log(error)
+        if (error instanceof ErrorEvent) {
+          console.error('An error occurred:', error);
+        } else {
+          // The backend returned an unsuccessful response code.
+          console.error(`Server returned status code ${error.status}, error message: ${error.message}`);
+
+        }
+      }
+    })
+    // this.service.getAllProductsWithLimit()
+    // .subscribe({
+    //   next: (data) => {
+    //     console.log(data)
+        
+    //   }
+    // });
   }
 
   // ngOnInit(): void {
--- src/app/services/api/products/product.service.ts
+++ src/app/services/api/products/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { ProductRepresentation } from '../models/product-representation';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,14 @@ export class ProductService {
     private http: HttpClient
   ) { }
 
-  getAllProductsWithLimit(limit: number = 5) {
+  getAllProductsWithLimit(limit:number = 5) {
     // const productUrl:string = this.baseUrl + 'products?limit=' + 5;
-    const productsUrl:string = `${this.baseUrl}products?limit='${limit}`;
-    return this.http.get(productsUrl)
+    const productsUrl:string = `${this.baseUrl}products?limit=${limit}`;
+    return this.http.get<ProductRepresentation[]>(productsUrl)
+    //or return this.http.get<Array<ProductRepresentation>>(productsUrl)
+  }
+  createProduct(product: ProductRepresentation) {
+    const productsUrl: string = `${this.baseUrl}productsddd`
+    return this.http.post<ProductRepresentation>(productsUrl, product)
   }
 }
